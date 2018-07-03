7b37bec9654bbc1b368196cd19a6b67bd8ef6400
Peter Hebert <peter@peterhebert.com>
2018-07-03 12:41:37 -0700

    add bootstrap classes to comment_reply_link

--- inc/misc.php
+++ inc/misc.php
@@ -18,7 +18,7 @@ function bootstrap_twig_clean_phone($str) {
     } catch (\libphonenumber\NumberParseException $e) {
         log_error($e);
     }
-    
+
 }
 
 function bootstrap_twig_format_phone($str) {
@@ -35,4 +35,23 @@ function bootstrap_twig_format_phone($str) {
 
 function bootstrap_twig_url_without_protocol($url) {
     return parse_url($url, PHP_URL_HOST);
+}
+
+// add bootstrap classes to comment_reply_link
+add_filter( 'comment_reply_link', 'bootstrap_twig_filter_comment_reply_link', 10, 4 );
+function bootstrap_twig_filter_comment_reply_link( $html, $args, $comment, $post ) {
+
+    return preg_replace_callback('/class=\'([a-z- ]+)\'/', function($m) {
+
+    $pattern = "/\bcomment-reply-link\b/" ;
+    $replacement = "btn btn-sm btn-secondary";
+
+    if(strpos($m[1], "comment-reply-link") !== false) {
+        $m[0] = preg_replace($pattern, $replacement,$m[0],1);
+    }
+
+    return $m[0];
+
+       }, $html);
+
 }
