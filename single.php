ef017ce1475008445a9147424dc789cfe3bac1a3
Peter Hebert <peter@peterhebert.com>
2018-07-03 12:41:11 -0700

    check for comment ID in url path for single template

--- single.php
+++ single.php
@@ -13,6 +13,14 @@ $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 
+// see if there is a comment id in the url
+if( isset( $_GET['replytocom'] ) ) {
+	$context['reply_to_comment'] = intval($_GET['replytocom']);
+}
+
+// get blog widgets
+$data['blog_widgets'] = Timber::get_widgets( 'blog-widgets' );
+
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
