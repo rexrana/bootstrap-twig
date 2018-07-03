c236379435ae177818d4f70fd6039b3f147fe581
Peter Hebert <peter@peterhebert.com>
2018-07-03 13:00:18 -0700

    updated page twig template suggestions
    to include post id template

--- page.php
+++ page.php
@@ -24,4 +24,9 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
+$templates = array( 
+    'page-' . $post->post_name . '.twig', 
+    'page-' . $post->id . '.twig', 
+    'page.twig'
+);
+Timber::render( $templates, $context );
